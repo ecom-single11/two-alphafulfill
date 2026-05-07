416aa3ea6b6cb3b93323b4f1abfebe12d0b641b9
ecom-single11 <ecomgithub@gmail.com>
2026-05-07 10:58:41 +0530

    Delete API directory

--- API/routes/aboutUsRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router  = express.Router();
-const ctrl    = require('../controllers/aboutUsController');
-const { auth } = require('../middlewares/auth.js');
-
-router.get('/get',    ctrl.get);           // public
-router.post('/upsert', auth, ctrl.upsert); // admin only
-
-module.exports = router;
